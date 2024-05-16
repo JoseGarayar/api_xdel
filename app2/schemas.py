8b24e11aa32b3d4ac127c05b41e095aa930c9120
Brunoware <bruno.miranda@pucp.edu.pe>
2024-05-15 22:11:10 -0500

    [UPDATE] App2 with todo

--- app2/schemas.py
+++ app2/schemas.py
@@ -4,6 +4,7 @@ from __init__ import api
 input_customer_schema = api.model('Input_Customer', {
     'type_id': fields.Integer(description='The type ID of the customer'),
     'name': fields.String(required=True, description='The name of the customer'),
+    'email': fields.String(required=True, description='The email of the customer'),
     'company': fields.String(description='The company of the customer'),
 })
 
@@ -11,8 +12,8 @@ output_customer_schema = api.model('Output_Customer', {
     'customer_id': fields.Integer(description='The customer ID'),
     'type_id': fields.Integer(description='The type ID of the customer'),
     'name': fields.String(required=True, description='The name of the customer'),
-    'company': fields.String(description='The company of the customer'),
-    'default_address_id': fields.Integer(description='The default address ID of the customer')
+    'email': fields.String(required=True, description='The email of the customer'),
+    'company': fields.String(description='The company of the customer')
 })
 
 input_address_schema = api.model('Input_Address', {
@@ -23,11 +24,7 @@ input_address_schema = api.model('Input_Address', {
     'suburb': fields.String(description='The suburb'),
     'city': fields.String(description='The city'),
     'state': fields.String(description='The state'),
-    'country': fields.String(description='The country'),
-    'email': fields.String(description='The email address'),
-    'phone': fields.String(description='The phone number'),
-    'sms_enabled': fields.Boolean(description='Indicates whether SMS is enabled'),
-    'vat_tax_id': fields.String(description='The VAT tax ID')
+    'country': fields.String(description='The country')
 })
 
 output_address_schema = api.model('Output_Address', {
@@ -39,9 +36,5 @@ output_address_schema = api.model('Output_Address', {
     'suburb': fields.String(description='The suburb'),
     'city': fields.String(description='The city'),
     'state': fields.String(description='The state'),
-    'country': fields.String(description='The country'),
-    'email': fields.String(description='The email address'),
-    'phone': fields.String(description='The phone number'),
-    'sms_enabled': fields.Boolean(description='Indicates whether SMS is enabled'),
-    'vat_tax_id': fields.String(description='The VAT tax ID')
+    'country': fields.String(description='The country')
 })
